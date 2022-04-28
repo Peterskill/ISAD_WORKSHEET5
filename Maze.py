4566e2c602718146030ee3ce690b7ce496944ef9
S.P.Shan <20906531@student.curtin.edu.au>
2022-04-28 09:01:43 +0530

    New Grid Update

--- Maze.py
+++ Maze.py
@@ -1,3 +1,5 @@
+import GridViewer
+
 EMPTY = 0
 WALL = 1
 START = 2
@@ -24,23 +26,7 @@ if __name__ == "__main__":
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             
-            if grid[i][j] == EMPTY:
-                print("  ", end = "")
-                    
-            elif grid[i][j] == WALL:
-                print("##", end = "")
-                    
-            elif grid[i][j] == START:
-                print("^^", end = "")
-                    
-            elif grid[i][j] == END:
-                print("$$", end = "")
-                    
-            elif grid[i][j] == VISITED:
-                print("..", end = "")
-                    
-            else:
-                raise AssertionError
+            GridViewer.view(grid)
             
         print()
 
@@ -111,22 +97,6 @@ if __name__ == "__main__":
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             
-            if grid[i][j] == EMPTY:
-                print("  ", end = "")
-                    
-            elif grid[i][j] == WALL:
-                print("##", end = "")
-                    
-            elif grid[i][j] == START:
-                print("^^", end = "")
-                    
-            elif grid[i][j] == END:
-                print("$$", end = "")
-                    
-            elif grid[i][j] == VISITED:
-                print("..", end = "")
-                    
-            else:
-                raise AssertionError
+            GridViewer.view(grid)
             
         print()
